3087d9c7b121efd993549ab1952f398b99385baa
wayne <52194625+wayne-oo@users.noreply.github.com>
2023-05-11 16:12:31 +0800

    seeson 3.1  streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
